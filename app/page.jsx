e6407d005a31ba2de6f846b07144ed50acd77afa
Jace Cabrera <jcezarcabrera@gmail.com>
2024-03-31 19:32:40 -0700

    Add initial project files and configuration

---
+++ app/page.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const homepage = () => {
+  return (
+    <div>
+      <h1>homepage</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aspernatur
+        deserunt a vitae quod obcaecati aperiam, error, molestias eius at,
+        consectetur repellat placeat. Error, consequuntur aliquam?
+      </p>
+    </div>
+  );
+};
+
+export default homepage;
